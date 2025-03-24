91896f74a1cf9667452bf34a522f44b45cf08bdf
Jordan-cybergeek <jordankafundawetou@gmail.com>
2025-03-24 11:50:07 +0000

    correction for add specific privecy policy to site for trusted script download

--- myApp.js
+++ myApp.js
@@ -16,7 +16,7 @@ app.use(helmet.noCache());
 app.use(
   helmet.contentSecurityPolicy({
     directives: {
-      defaultSrc: ["'self"],
+      defaultSrc: ["'self'"],
         scriptSrc:["'self'",'trusted-cdn.com']
     }
   })
